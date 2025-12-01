888d92d82d074fcbcaf55b08a741d8fecfb0fcc9
Huntpkmn <101064040+Huntpkmn@users.noreply.github.com>
2025-12-01 16:38:45 -0600

    Add self histories

--- SimulationFramework/agents/abstract_agent.py
+++ SimulationFramework/agents/abstract_agent.py
@@ -1,11 +1,17 @@
 from abc import ABC
+from enums.choices import DescisionTranscript
 
 class AbstractAgent(ABC):
 
     def __init__(self, name:str):
         self._name = name
+        self.history= DescisionTranscript()
         if len(name) == 0:
             raise ValueError("Name cannot be empty!")
+    
+    def get_history(self):
+        return self.history
+ 
 
     def decide(self, lore):
         pass
